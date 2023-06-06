1aa6a68b956313a75f98435188129edd769aa857
Bckforgood <t.abdo.khbaza@gmail.com>
2023-06-06 10:32:00 -0700

    chmod upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
+    output = ""
     for c in str:
         ascii_value = ord(c)
         if ascii_value >= 97 and ascii_value <= 122:
             uppercase_value = ascii_value - 32
-            print("{}".format(chr(uppercase_value)), end="")
+            output += "{}".format(chr(uppercase_value))
         else:
-            print("{}".format(c), end="")
-    print()
+            output += "{}".format(c)
+    print(output)
