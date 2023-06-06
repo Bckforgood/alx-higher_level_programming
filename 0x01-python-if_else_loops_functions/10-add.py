6c6d8a283bb370ce36fd84e27fa70f83ca9bbef0
Bckforgood <t.abdo.khbaza@gmail.com>
2023-06-06 15:20:30 -0700

    add

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    while b != 0:
-        sum_without_carry = a ^ b
-        carry = (a & b) << 1
-        a = sum_without_carry
-        b = carry
-    return a
+    """Return the addition of a and b."""
+    return (a + b)
