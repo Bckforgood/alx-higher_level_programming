3847bd634e77e6031c0a0ec020582cb2351d1959
Bckforgood <t.abdo.khbaza@gmail.com>
2023-06-08 10:54:14 -0700

    push all of mandatory task

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import hidden_4
+
+def print_hidden_names():
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
+
+if __name__ == "__main__":
+    print_hidden_names()
