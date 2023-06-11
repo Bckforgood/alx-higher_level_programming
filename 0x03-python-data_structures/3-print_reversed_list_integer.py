d666295cadf42309b672022031838f2595c6e066
Bckforgood <t.abdo.khbaza@gmail.com>
2023-06-11 13:16:24 -0700

    reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
-        print("", e="")
+        print("", end="")
     else:
         my_list.reverse()
         for i in my_list:
