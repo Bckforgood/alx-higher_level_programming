9a039f7ac11d96a416aa10857baa7204d8c7c3dd
Bckforgood <t.abdo.khbaza@gmail.con>
2023-07-03 10:32:01 -0700

    testpush

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+>>> Rectangle = __import__('0-rectangle').Rectangle
+
+>>> my_rectangle = Rectangle()
+>>> print(type(my_rectangle))
+<class '0-rectangle.Rectangle'>
+
+>>> print(my_rectangle.__dict__)
+{}
+
+
+
 class Rectangle:
-    def __init__(self):
-        pass
-
-    def calculate_area(self):
-        pass
-
-    def calculate_perimeter(self):
-        pass
-
-    def is_square(self):
-        pass
+    pass
