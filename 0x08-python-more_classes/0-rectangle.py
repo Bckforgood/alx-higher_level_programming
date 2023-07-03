b1eabd41c2a483508c0b5951eda3a5c7a4440a6a
Bckforgood <t.abdo.khbaza@gmail.con>
2023-07-03 10:30:41 -0700

    testpush

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self):
+        pass
+
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+    def is_square(self):
+        pass
