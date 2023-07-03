52858021988e279f6ee717d2edb165ce60a659a5
Bckforgood <t.abdo.khbaza@gmail.con>
2023-07-03 10:32:47 -0700

    testpush

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+# 0-rectangle module
+"""
+This module contains a class Rectangle
+
 >>> Rectangle = __import__('0-rectangle').Rectangle
 
 >>> my_rectangle = Rectangle()
@@ -8,7 +13,9 @@
 >>> print(my_rectangle.__dict__)
 {}
 
+"""
 
 
 class Rectangle:
+    """This is an empty class"""
     pass
