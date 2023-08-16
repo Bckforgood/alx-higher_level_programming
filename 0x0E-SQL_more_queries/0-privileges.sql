2bfc815968f02bc48d0d7a0a539192b70ff6a20d
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-16 16:54:57 -0700

    first push

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Task 2: Verify privileges for users
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
