36ac81f4b6d70f62c5aec391a390e326dff53dae
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-16 17:04:47 -0700

    fix some errors in task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,10 +1,11 @@
--- Create user user_0d_2
+-- Create user user_0d_1 and grant privileges
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user user_0d_2 and grant SELECT privilege
 CREATE USER 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
 
--- Grant privileges to user_0d_2 (adjust privileges as needed)
-GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'user_0d_2'@'localhost';
-
--- List privileges for both users
+-- List privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
