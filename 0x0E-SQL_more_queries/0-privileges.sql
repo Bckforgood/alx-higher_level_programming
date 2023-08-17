bdda0f548a3bd118ad98b52b9163d9c93cf2c9b3
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-17 04:45:16 -0700

    fix errors

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- Create user user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- show privs for user1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show privs for user2
+SHOW GRANTS FOR user_0d_2@localhost;
