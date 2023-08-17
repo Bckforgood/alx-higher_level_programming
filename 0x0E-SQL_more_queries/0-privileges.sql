62191c14cdce3ffee0358706c741cd10da1061b3
Bckforgood <t.abdo.khbaza@gmail.con>
2023-08-16 17:03:21 -0700

    fix some errors in task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,10 @@
--- Task 2: Verify privileges for users
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Create user user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
 
--- List privileges for user_0d_2
+-- Grant privileges to user_0d_2 (adjust privileges as needed)
+GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'user_0d_2'@'localhost';
+
+-- List privileges for both users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
