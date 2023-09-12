ecea57fe37428edafb47e99445d782bd7f1836f6
Bckforgood <t.abdo.khbaza@gmail.con>
2023-09-12 14:34:40 -0700

    fix requirement

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-// Use 'let' and 'const' instead of 'var'
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
 
 module.exports = Rectangle;
-
