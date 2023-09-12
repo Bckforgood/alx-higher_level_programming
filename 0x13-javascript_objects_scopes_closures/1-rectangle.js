88c747ee2d18898bf47164cbd9c8f7d542901cd2
Bckforgood <t.abdo.khbaza@gmail.con>
2023-09-12 14:23:36 -0700

    first push

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+
+// Use 'let' and 'const' instead of 'var'
 class Rectangle {
   constructor(w, h) {
     this.width = w;
     this.height = h;
   }
 }
+
 module.exports = Rectangle;
+
