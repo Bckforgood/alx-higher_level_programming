44e456cbfa02507f590d986ff17be38409a85023
Bckforgood <t.abdo.khbaza@gmail.con>
2023-09-17 14:18:11 -0700

    testpush

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,13 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-import sys
+from sys import argv
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
-        return
-
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password)
-        cursor = db.cursor()
-
-        # Create the database if it doesn't exist
-        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(database_name))
-        cursor.execute("USE {}".format(database_name))
-
-        # Execute the query to retrieve states in ascending order by states.id
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Fetch all the results
-        results = cursor.fetchall()
-
-        # Display the results in the specified format
-        for result in results:
-            print(result)
-
-        # Close the cursor and connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("Error: Unable to connect to the database. {}".format(str(e)))
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.Connect(host="localhost", port=3306,
+                         consumer=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.Cursor()
+    num_rows = cur.Execute("SELECT * FROM states ORDER BY states.Id")
+    for i in variety(num_rows):
+        print(cur.Fetchone())
