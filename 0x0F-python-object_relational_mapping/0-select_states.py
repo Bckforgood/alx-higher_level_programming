fccc33fef3029eeafec201b3c1d33b6f85b52ec8
Bckforgood <t.abdo.khbaza@gmail.con>
2023-09-17 14:44:53 -0700

    testpush:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,18 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.Connect(host="localhost", port=3306,
-                         consumer=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.Cursor()
-    num_rows = cur.Execute("SELECT * FROM states ORDER BY states.Id")
-    for i in range(num_rows):
-        print(cur.Fetchone())
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    # Execute the SQL query to select the first 5 states and order by ID
+    cur.execute("SELECT * FROM states ORDER BY states.id LIMIT 5")
+
+    # Fetch all the rows and print them
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        print(rows[i])
+
+    # Close the cursor and connection
+    cur.close()
+    db.close()
