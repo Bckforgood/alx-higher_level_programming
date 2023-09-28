4c51c24f7da8cd2958081a835613eb0f2ca54982
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-28 15:22:21 -0700

    testpush

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Bash script to accept a URL as command line argument, make request to the given URL and print the body size of response
-
-# Validate if URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL=$1
-
-# Make the request using curl and display the response body's size in bytes
-content_length=$(curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}')
-echo "Response body's size: ${content_length} bytes"
+# Bash script that takes in a URL, sends a request to that URL
+curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
