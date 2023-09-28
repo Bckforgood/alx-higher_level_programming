293f3d41206f6fc77f8256e50d0e8f77dfb00213
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-28 15:09:54 -0700

    test

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script takes a URL, sends a request using curl, and displays the size of the response body in bytes
+# This script takes a URL and displays the size of the response body in bytes
 
 # Check if URL is provided
 if [ -z "$1" ]; then
@@ -10,6 +10,5 @@ fi
 
 URL=$1
 
-# Send the request using curl, follow redirects, and display the size of the response body in bytes
+# Send the request and display the size of the response body in bytes
 curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}'
-
