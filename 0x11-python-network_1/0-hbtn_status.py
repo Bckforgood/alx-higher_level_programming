f9e8f22cf89c99d9e4b4b970d77f417020dc8222
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-29 09:33:24 -0700

    fix errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
+"""
+Python script that fetches a URL using urllib package
+and displays the body response in a specified format.
+"""
+
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+def fetch_and_display_response(url):
+    """
+    Fetches the URL using urllib and displays the body response.
+    
+    Parameters:
+        url (str): The URL to fetch.
+    """
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
 
         print("Body response:")
         print("    - type: {}".format(type(content)))
-        print("    - content: {}".format(content))
+        print("    - content: {}".format(content.decode('utf-8')))
         print("    - utf8 content: {}".format(utf8_content))
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_and_display_response(url)
