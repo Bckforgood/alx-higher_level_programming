100b86f755fbc507878b85c78335cd4f7acf330a
Bckforgood <t.abdo.khbaza@gmail.com>
2023-09-29 09:34:32 -0700

    fix errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that fetches a URL using urllib package
-and displays the body response in a specified format.
-"""
-
-import urllib.request
-
-def fetch_and_display_response(url):
-    """
-    Fetches the URL using urllib and displays the body response.
-    
-    Parameters:
-        url (str): The URL to fetch.
-    """
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
-
-        print("Body response:")
-        print("    - type: {}".format(type(content)))
-        print("    - content: {}".format(content.decode('utf-8')))
-        print("    - utf8 content: {}".format(utf8_content))
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    fetch_and_display_response(url)
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
