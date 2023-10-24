ebb4173f500329fbbffa7ab7832837cd7d7c912f
Bckforgood <t.abdo.khbaza@gmail.com>
2023-10-24 16:12:13 -0700

    test

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Check if both the file path and content are provided as arguments
-if (!filePath || !content) {
-  console.error('Usage: ./1-writeme.js <file_path> <content>');
-  process.exit(1);
-}
-
-// Write the content to the file in utf-8 encoding
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err); // Print the error object if an error occurs
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
-
